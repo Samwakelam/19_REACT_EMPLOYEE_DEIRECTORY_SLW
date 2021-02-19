842df90738b7edbf875fd65676760b3f45c5405a
Improbdrive <sam.wakelam@hotmail.com>
2021-02-19 15:35:16 +0000

    filter by name function added

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,21 @@
 import './App.css';
+import { useState } from 'react';
 import MyForm from '../MyForm/MyForm.js';
 import MyTable from '../MyTable/MyTable.js';
 
 function App() {
+
+  const [inputFilter, setInputFilter] = useState('');
+
+  const handleInputChange = (value) => {
+    setInputFilter(value);
+    // console.log('App.js, value =',value);
+  }
+
   return (
     <div className="App">
-      <MyForm />
-      <MyTable />
+      <MyForm onInputChange={handleInputChange}/>
+      <MyTable filter={inputFilter}/>
     </div>
   );
 }
