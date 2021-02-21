14e29700831de5cc117bba0f951fce4068aa29e6
Improbdrive <sam.wakelam@hotmail.com>
2021-02-21 10:32:07 +0000

    change the filter field

--- src/MyForm/MyForm.js
+++ src/MyForm/MyForm.js
@@ -1,9 +1,17 @@
 import './MyForm.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-const MyForm = ({ onInputChange, onSelectChange, sort, onReverse }) => {
+const MyForm = ({ 
+  onInputChange, 
+  onSelectChange, 
+  onChangeFilter, 
+  sort, 
+  onReverse, 
+  filterBy
+}) => {
 
   // const [inputFilter, setInputFilter] = useState('');
+  const [field, setField] = useState('Name');
 
   const inputChange = (event) => {
     // console.log(event.target.value);
@@ -14,27 +22,51 @@ const MyForm = ({ onInputChange, onSelectChange, sort, onReverse }) => {
     onSelectChange(event.target.value, 'sort');
   }
 
+  const changeFilter = (event) => {
+    onChangeFilter(event.target.value, 'filterBy');
+    // console.log(event.target.value);
+    if(event.target.value === 'name') {
+      setField('Name');
+    }
+    if(event.target.value === 'age') {
+      setField('Age');
+    }
+    if(event.target.value === 'dob') {
+      setField('Month');
+    }
+  }
+
   const reverseButton = (event) => {
     event.preventDefault();
     event.stopPropagation();
     console.log('click');
-    onReverse(true); 
+    onReverse(true);
   }
+
+  useEffect(() => {
+    
+  }, [field])
 
   return (
     <form>
-      <label htmlFor='filter-text' >Filter Employees:</label>
-      <input id='filter-text' type="text" onChange={inputChange} placeholder='Search Name' />
+      <label htmlFor='filter-text' >Filter Employees By:</label>
+      <label htmlFor='sort-category' className='hide'>Sort Employees:</label>
+      <select id="sort-filter" value={filterBy} name="filter-by" onChange={changeFilter}>
+        <option value="name">Name</option>
+        <option value="dob">DOB</option>
+        <option value="age">Age</option>
+      </select>
+      <input id='filter-text' type="text" onChange={inputChange} placeholder={`Search ${field}`} />
 
       <label htmlFor='sort-category' className='hide'>Sort Employees:</label>
-      <select value={sort} name="Sort By:" id="sort-category" onChange={selectChange}>
+      <select id="sort-category" value={sort} name="sort-by" onChange={selectChange}>
         <option value="sortBy">Sort By:</option>
         <option value="age">Age</option>
         <option value="dob">DOB</option>
         <option value="name">Name</option>
       </select>
 
-      <button id='up-down' type="button"><img src={`${process.env.PUBLIC_URL}/assets/img/up-down.png`} alt='up-down arrows' onClick={reverseButton}/></button>
+      <button id='up-down' type="button"><img src={`${process.env.PUBLIC_URL}/assets/img/up-down.png`} alt='up-down arrows' onClick={reverseButton} /></button>
 
     </form>
   );
